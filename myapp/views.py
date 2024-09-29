16b4685e99197bb58f9ad6270b2b5693be11f0b8
wasabina67 <wasabina67@gmail.com>
2024-09-29 16:34:45 +0900

    Update myapp/apps.py, myapp/views.py, prj/urls.py & Create myapp/templates/index.html, myapp/urls.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Person
 
-# Create your views here.
+
+def index(request):
+    return render(request, "index.html", {"persons": Person.objects.all()})
