3ab333356747a6b75a7d6987e96b1f7e62bde928
wasabina67 <wasabina67@gmail.com>
2024-09-29 17:20:59 +0900

    Update Person model

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    age = models.IntegerField(default=0)
+    deleted = models.BooleanField(default=False)
