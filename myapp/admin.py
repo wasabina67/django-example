6bfca9955d84720d558641a4347a2b68fa624e4a
wasabina67 <wasabina67@gmail.com>
2024-09-29 17:28:34 +0900

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -4,6 +4,9 @@ from .models import Person
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ["id", "first_name", "last_name", "age", "deleted"]
+    list_filter = ["deleted"]
+    list_per_page = 5
+    search_fields = ["first_name", "last_name"]
 
 
 admin.site.register(Person, PersonAdmin)
