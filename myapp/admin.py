38747f90e83cae7f7e92afe507e3639c0a34b732
wasabina67 <wasabina67@gmail.com>
2024-09-29 17:45:02 +0900

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,21 @@
+from django import forms
 from django.contrib import admin
 from .models import Person
 
 
+class PersonForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = "__all__"
+        widgets = {
+            "first_name": forms.TextInput(attrs={"autocomplete": "off"}),
+            "last_name": forms.TextInput(attrs={"autocomplete": "off"}),
+        }
+
+
 class PersonAdmin(admin.ModelAdmin):
+    form = PersonForm
+
     list_display = ["id", "first_name", "last_name", "age", "deleted"]
     list_filter = ["deleted"]
     list_per_page = 5
